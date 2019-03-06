7e937322ac40132cb8efdb1f3794554c0956c881
bmanek <41875460+bmanek@users.noreply.github.com>
2019-03-06 08:46:36 -0500

    update and clean dummy seed data

--- seed.rb
+++ seed.rb
@@ -1,17 +1,17 @@
-#### Brand
-chumley = Brand.new("Chumley's")
-murph = Brand.new("Murph's Bilgewater")
-bloggerfeld = Brand.new("Bloggerfeld's Family Moonshine")
-mupley = Brand.new("Mupley's Discount Rotgut")
+# #### Brand
+# chumley = Brand.new("Chumley's")
+# murph = Brand.new("Murph's Bilgewater")
+# bloggerfeld = Brand.new("Bloggerfeld's Family Moonshine")
+# mupley = Brand.new("Mupley's Discount Rotgut")
 
 #### Release
-old_timey = Release.new(chumley, "Chumley's Old Timey Likker", 8.0)
-new_timey = Release.new(chumley, "Chumley's New Timey Likker", 10.0)
-water = Release.new(murph, "Murph's Barely Better'n Water", 5.0)
-better = Release.new(murph, "Murph's Better Likker", 8.0)
-hi_fashin = Release.new(bloggerfeld, "Fashinibble Rotgut", 50.0)
-muppet = Release.new(mupley, "Mupley's Ragin' Muppet", 12.0)
-dumpster_fire = Release.new(mupley, "Dumpster Fire", 25.0)
+old_timey = Release.new("Chumley's Old Timey Likker", "Chumley's" 8.0)
+new_timey = Release.new("Chumley's New Timey Likker", "Chumley's" 10.0)
+water = Release.new("Murph's Barely Better'n Water", "Murph's Bilgewater" 5.0)
+better = Release.new("Murph's Better Likker", "Murph's Bilgewater", 8.0)
+hi_fashin = Release.new("Fashinibble Rotgut", "Bloggerfeld's Family Moonshine" 50.0)
+muppet = Release.new("Mupley's Ragin' Muppet", "Mupley's Discount Rotgut", 12.0)
+dumpster_fire = Release.new("Dumpster Fire", "Mupley's Discount Rotgut", 25.0)
 
 #### User
 
@@ -23,12 +23,12 @@ frank = User.new("Frank")
 
 
 #### Review
-middling = Review.new(frank, old_timey, 56, "I've had better.")
-acceptable = Review.new(frank, new_timey, 62, "Better than the old one.")
-mediocre = Review.new(frank, water, 50, "The label's right on the money.")
-surprising = Review.new(dennis, hi_fashin, 80, "Sophisticated.")
-magnificent = Review.new(charlie, muppet, 97, "Transcendant.")
-the_worst = Review.new(dee, water, 20, "Just the worst.")
-trash = Review.new(dee, dumpster_fire, 20, "Literally trash.")
-filty = Review.new(mac, dumpster_fire, 10, "Straight from the gutter.")
-weird = Review.new(mac, muppet, 90, "Weirdly good.")
+middling = Review.new(56, old_timey, frank, "I've had better.")
+acceptable = Review.new(62, new_timey, frank, "Better than the old one.")
+mediocre = Review.new(50, water, frank, "The label/'s right on the money.")
+surprising = Review.new(80, hi_fashin, dennis, "Sophisticated.")
+magnificent = Review.new(97, muppet, charlie, "Transcendant.")
+the_worst = Review.new(20, water, dee,   "Just the worst.")
+trash = Review.new(20, dumpster_fire, dee, "Literally trash.")
+filty = Review.new(10, dumpster_fire, mac, "Straight from the gutter.")
+weird = Review.new(90, muppet, mac, "Weirdly good.")
