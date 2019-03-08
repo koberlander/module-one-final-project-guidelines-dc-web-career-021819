8883ea0eaca34c453f5b755cd32788a5761a7c17
bmanek <41875460+bmanek@users.noreply.github.com>
2019-03-08 08:54:59 -0500

    add methods and method notes to release.rb

--- lib/release.rb
+++ lib/release.rb
@@ -2,4 +2,24 @@ class Release < ActiveRecord::Base
   has_many :reviews
   has_many :users, through: :reviews
 
+  # find price given Release
+  # CLI
+
+  # choose whiskey by brand (thru CLI)
+  def self.choose_by_brand(input)
+    self.find_by brand: input
+
+  # find review:score given release
+  #  CLI
+
+  # find review:content given release
+  # CLI
+
+  # enter a price range and get whiskeys within range
+  # select price range option thru CLI
+  # price = 1..10
+  # price = 10..20
+  # price = 20..100
+  #
+
 end
